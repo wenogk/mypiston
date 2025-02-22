b063f760e047ef621aff513b557da4137d817a57
Romeno Wenogk Fernando <wenogk@gmail.com>
2025-02-22 17:14:19 -0500

    fix java test

--- packages/java/15.0.2/test.java
+++ packages/java/15.0.2/test.java
@@ -1,6 +1,6 @@
 import org.json.JSONObject;
 
-public class HelloWorld {
+public class Test {
     public static void main(String[] args) {
         // Create a simple JSON object
         JSONObject json = new JSONObject();
