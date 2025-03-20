a387264c27a4d45706ffae4a154ecbb1dece3466
Romeno Wenogk Fernando <wenogk@gmail.com>
2025-03-19 23:06:46 -0400

    Add datastructures js

--- packages/node/20.11.1/build.sh
+++ packages/node/20.11.1/build.sh
@@ -2,3 +2,5 @@
 curl "https://nodejs.org/dist/v20.11.1/node-v20.11.1-linux-x64.tar.xz" -o node.tar.xz
 tar xf node.tar.xz --strip-components=1
 rm node.tar.xz
+
+npm install --save datastructures-js/stack datastructures-js/queue datastructures-js/deque datastructures-js/linked-list datastructures-js/set datastructures-js/heap datastructures-js/priority-queue datastructures-js/binary-search-tree datastructures-js/trie datastructures-js/graph
