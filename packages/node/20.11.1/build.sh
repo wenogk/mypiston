fe72a2cccffcb0e97cc4efc3527d4124df2699d9
Romeno Wenogk Fernando <wenogk@gmail.com>
2025-03-22 07:37:50 -0400

    export bin path

--- packages/node/20.11.1/build.sh
+++ packages/node/20.11.1/build.sh
@@ -2,5 +2,6 @@
 curl "https://nodejs.org/dist/v20.11.1/node-v20.11.1-linux-x64.tar.xz" -o node.tar.xz
 tar xf node.tar.xz --strip-components=1
 rm node.tar.xz
+export PATH=$PWD/bin:$PATH
 # install datastructures js packages
 npm install --save datastructures-js/stack datastructures-js/queue datastructures-js/deque datastructures-js/linked-list datastructures-js/set datastructures-js/heap datastructures-js/priority-queue datastructures-js/binary-search-tree datastructures-js/trie datastructures-js/graph
