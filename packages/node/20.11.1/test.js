6f4505c2228b87ab47a956a2dc75b40498b6bd9e
Romeno Wenogk Fernando <romenowenogk@gmail.com>
2025-03-25 19:27:32 -0400

    bin/npm (#14)
    * Fix bug with js datastructures package installation and use them in test
    
    * empty commit
    
    * fix bug
    
    ---------
    
    Co-authored-by: Romeno Wenogk Fernando <wenogk@gmail.com>

--- packages/node/20.11.1/test.js
+++ packages/node/20.11.1/test.js
@@ -1,1 +1,29 @@
-console.log('OK');
+const { Stack } = require('@datastructures-js/stack');
+const { Queue } = require('@datastructures-js/queue');
+const { LinkedList } = require('@datastructures-js/linked-list');
+
+// Create a stack and perform some operations
+const stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.pop();
+const stackTop = stack.peek(); // Get the top element of the stack
+
+// Create a queue and perform some operations
+const queue = new Queue();
+queue.enqueue(1);
+queue.enqueue(2);
+queue.dequeue();
+const queueFront = queue.front(); // Get the front element of the queue
+
+// Create a linked list and perform some operations
+const linkedList = new LinkedList();
+linkedList.insertFirst(1);
+linkedList.insertLast(2);
+linkedList.removeFirst();
+const linkedListFirst = linkedList.head() ? linkedList.head().getValue() : -1; // Get the first element of the linked list
+
+// Check the states of the data structures
+if (stackTop === 1 && queueFront === 2 && linkedListFirst === 2) {
+  console.log('OK');
+}
